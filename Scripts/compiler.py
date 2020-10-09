7bf51bd9f82e3d49841924a0052585f642c8be27
Neeraj Deshmukh <neeraj.deshmukh@students.iiserpune.ac.in>
2020-10-09 14:19:14 +0530

    October 9 Slides lecture version

--- Scripts/compiler.py
+++ Scripts/compiler.py
@@ -10,4 +10,4 @@ subprocess.check_call(['bibtex', 'tag_notes'])
 subprocess.check_call(['pdflatex', 'tag_notes.tex'])
 subprocess.check_call(['pdflatex', 'tag_notes.tex'])
 
-shutil.move("tag_notes.pdf", "notes.pdf")
+shutil.move("tag_notes.pdf", "Notes.pdf")
